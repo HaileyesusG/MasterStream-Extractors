a261ddf682d18b7cc9c86e49db5b1600773fa10e
HAILEYESUS GETNET <114768975+HaileyesusG@users.noreply.github.com>
2026-07-08 14:11:16 +0300

    Reorder server list in VideasyExtractor

--- extractors/VideasyExtractor.js
+++ extractors/VideasyExtractor.js
@@ -91,7 +91,7 @@
 
       // cdn (Yoru) is movies-only with built-in subtitles and possible 4K → try it FIRST for movies.
       // TV series will skip it automatically (guard at line below) and fall through to jett.
-      var servers = ['neon2', 'cdn', 'ym', 'tejo', 'jett', 'downloader2', 'm4uhd', 'hdmovie'];
+      var servers = ['downloader2', 'cdn', 'neon2', 'tejo', 'jett', 'ym', 'm4uhd', 'hdmovie'];
 
       for (var i = 0; i < servers.length; i++) {
         var server = servers[i];
